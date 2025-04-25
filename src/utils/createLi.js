96fd99fb42c43fe1dc8b253eae80302777f2729f
Mariam Oluseyi-Alli <mariam_oa@yahoo.com>
2025-04-25 22:06:40 +0100

    added checkboxes to the list functionality

--- src/utils/createLi.js
+++ src/utils/createLi.js
@@ -1,5 +1,6 @@
 import { createDeleteButton } from "./createDelBtn.js";
 import { addEditFunctionality } from "./editFunction.js";
+import { saveTodoToLocalStorage, getTodosFromLocalStorage } from "./saveTodos.js";
 
 // Function to create a new list item
 export function createListItem(todo) {
@@ -7,7 +8,27 @@ export function createListItem(todo) {
   listItem.classList.add("list");
   listItem.setAttribute("data-id", todo.id);
 
+  // Create a checkbox
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = todo.completed; // Set the checkbox state based on the todo's completed property
+  if (todo.completed) {
+    listItem.classList.add("completed"); // Add the completed class if the todo is already completed
+  }
+
+  checkbox.addEventListener("change", () => {
+    const todos = getTodosFromLocalStorage();
+    const index = todos.findIndex((t) => t.id === todo.id);
+    if (index !== -1) {
+      todos[index].completed = checkbox.checked;
+      saveTodoToLocalStorage(todos);
+      listItem.classList.toggle("completed", checkbox.checked); // Toggle the completed class
+    }
+  });
+
   const textNode = document.createTextNode(todo.text);
+
+  listItem.appendChild(checkbox); // Add the checkbox to the list item
   listItem.appendChild(textNode);
 
   // Add delete button
